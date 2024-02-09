c495f4eb51291e9c7eb298a3fd4fe24daf345e4d
v <v@valixym.net>
2024-02-09 14:16:44 -0500

    lots of stuff

--- vx.cr
+++ vx.cr
@@ -1,23 +1,23 @@
 fn = ARGV[0]
+tn = ARGV[1]
 
 SITE_CONFIG = {
   "url"     => "https://valixym.net",
   "contact" => "v@valixym.net",
   "repo"    => "https://github.com/valixym/valixym.net",
   "title"   => "VALIXYM",
+  "yao"     => ARGV[0],
 }
 
-FEATURED_IMAGE = {
-  "url"         => "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Raspberries05.jpg/1200px-Raspberries05.jpg?20060301080442",
-  "author"      => "Fir0002",
-  "license"     => "CC BY-NC",
-  "license_url" => "https://creativecommons.org/licenses/by-nc/2.0/",
-}
+# FEATURED_IMAGES = {
+#   "url1" => "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Raspberries05.jpg/1200px-Raspberries05.jpg?20060301080442",
+#   "url2" => "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Jonos-table-photo_edit.jpg/1200px-Jonos-table-photo_edit.jpg?20240104074741",
+# }
 
-content = File.read_lines("content/#{fn}.html")[2..-1].join
+content = File.read_lines("content/#{fn}.html")[2..-1].join("\n")
 page_title = File.read_lines("content/#{fn}.html")[0]
 
-template = File.read("templates/index.html")
+template = File.read("templates/#{tn}.html")
 xout = template.gsub("{% PAGE_CONTENT %}", content)
 xout = xout.gsub("{% PAGE_TITLE %}", page_title)
 
@@ -25,7 +25,7 @@ SITE_CONFIG.each do |cfg|
   xout = xout.gsub("{% site_#{cfg[0]} %}", cfg[1])
 end
 
-FEATURED_IMAGE.each do |cfg|
+FEATURED_IMAGES.each do |cfg|
   xout = xout.gsub("{% fm_#{cfg[0]} %}", cfg[1])
 end
 
