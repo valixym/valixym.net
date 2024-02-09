e51eb2285366cfa2e28b91fc344f9cea23573e36
v <v@valixym.net>
2024-02-09 15:09:46 -0500

    automatically build everything

--- vx.cr
+++ vx.cr
@@ -1,15 +1,3 @@
-fn = ARGV[0]
-tn = ARGV[1]
-
-SITE_CONFIG = {
-  "url"     => "https://valixym.net",
-  "contact" => "v@valixym.net",
-  "repo"    => "https://github.com/valixym/valixym.net",
-  "title"   => "VALIXYM",
-  "yao"     => ARGV[0],
-  "tagline" => "\"In Heaven, everything is fine\"",
-}
-
 FEATURED_IMAGE = {
   "url"         => "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Raspberries05.jpg/1200px-Raspberries05.jpg?20060301080442",
   "author"      => "Fir0002",
@@ -17,19 +5,38 @@ FEATURED_IMAGE = {
   "license_url" => "https://creativecommons.org/licenses/by-nc/2.0/",
 }
 
-content = File.read_lines("content/#{fn}.html")[2..-1].join("\n")
-page_title = File.read_lines("content/#{fn}.html")[0]
+def build_site(fn, tn)
+  site_config = {
+    "url"     => "https://valixym.net",
+    "contact" => "v@valixym.net",
+    "repo"    => "https://github.com/valixym/valixym.net",
+    "title"   => "VALIXYM",
+    "yao"     => fn,
+    "tagline" => "\"In Heaven, everything is fine\"",
+  }
 
-template = File.read("templates/#{tn}.html")
-xout = template.gsub("{% PAGE_CONTENT %}", content)
-xout = xout.gsub("{% PAGE_TITLE %}", page_title)
+  content = File.read_lines("content/#{fn}")[2..-1].join("\n")
+  page_title = File.read_lines("content/#{fn}")[0]
 
-SITE_CONFIG.each do |cfg|
-  xout = xout.gsub("{% site_#{cfg[0]} %}", cfg[1])
+  template = File.read("templates/#{tn}.html")
+  xout = template.gsub("{% PAGE_CONTENT %}", content)
+  xout = xout.gsub("{% PAGE_TITLE %}", page_title)
+
+  site_config.each do |cfg|
+    xout = xout.gsub("{% site_#{cfg[0]} %}", cfg[1])
+  end
+
+  FEATURED_IMAGE.each do |cfg|
+    xout = xout.gsub("{% fm_#{cfg[0]} %}", cfg[1])
+  end
+
+  File.write("dist/#{fn}", xout)
 end
 
-FEATURED_IMAGE.each do |cfg|
-  xout = xout.gsub("{% fm_#{cfg[0]} %}", cfg[1])
+Dir.new("content").entries.each do |cf|
+  if cf == "Layer14"
+    break
+  end
+  build_site(cf, "index")
+  puts cf
 end
-
-File.write("dist/#{fn}.html", xout)
